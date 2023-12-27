61ac2f8098949bd2a3aa07ba6ec8f5f43e20b977
Beza Mogese <beza.mogese@gmail.com>
2023-12-27 01:01:07 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import React, { useState, useRef } from 'react';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
+  const [timer, setTimer] = useState(0);
+  const [laps, setLaps] = useState([]);
+  const countRef = useRef(null);
+
+  const handleStart = () => {
+    countRef.current = setInterval(() => {
+      setTimer((timer) => timer + 1)
+    }, 10);
+  };
+
+  const handleStop = () => {
+    clearInterval(countRef.current);
+  };
+
+  const handleReset = () => {
+    clearInterval(countRef.current);
+    setTimer(0);
+    setLaps([]);
+  };
+
+  const handleLap = () => {
+    setLaps([...laps, timer]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt="logo"/>
+      <h1>Stopwatch</h1>
+      <span>{(timer/100).toFixed(2)}</span>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+      <button onClick={handleReset}>Reset</button>
+      <button onClick={handleLap}>Lap</button>
+      {laps.map(time => (
+        <p>{(time/100).toFixed(2)}</p>
+      ))}
+    </header>
     </div>
+    
   );
 }
 
